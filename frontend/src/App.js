cb94ed18551771f2d0aba5cedec150536845024a
nhungL <nhung.luong1098@gmail.com>
2022-03-12 20:28:47 -0800

    commit to checkout

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,10 +2,13 @@ import "./App.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import LoginPage from "./pages/LoginPage/LoginPage";
 import UnfoundPage from "./pages/UnfoundPage";
+import ResponsiveAppBar from "./components/ResponsiveAppBar";
 
 function App() {
   return (
     <div className="App">
+      <ResponsiveAppBar />
+      
       <BrowserRouter>
         <Routes>
           <Route path="/signin" element={<LoginPage />} />
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -7,8 +7,7 @@ import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    {/* <App /> */}
-    <ProfilePage />
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
