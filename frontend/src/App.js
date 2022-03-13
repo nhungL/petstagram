440ff287f6f4aa3d7babc74532a32f521bc3f5c0
nhungL <nhung.luong1098@gmail.com>
2022-03-12 19:53:32 -0800

    header

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,18 @@
 import "./App.css";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import React from "react";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import LoginPage from "./pages/LoginPage/LoginPage.js";
+import ProfilePage from "./pages/ProfilePage/ProfilePage";
 
 function App() {
   return (
-    <div className="App">
-      <Router>
-        <Route path="/signin" exact component={LoginPage} />
-      </Router>
-    </div>
+    <Router>
+      <Link className="nav-link" to="/profile">Course-Table</Link>
+      <h1>asdfasf</h1>
+      {/* <Route path="/signin" exact component={LoginPage} /> */}
+    
+      <Route path="/profile" component={ProfilePage} />
+    </Router>
   );
 }
 
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,11 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import ProfilePage from './pages/ProfilePage/ProfilePage';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
+    <ProfilePage />
   </React.StrictMode>,
   document.getElementById('root')
 );
