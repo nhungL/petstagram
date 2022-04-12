885cdcff74762f9038660103c386aa0947c65467
Thuy Le <phuongthuy9796@gmail.com>
2022-04-12 13:36:53 -0700

    Add register constants

--- frontend/src/components/SignUp/SignUp.js
+++ frontend/src/components/SignUp/SignUp.js
@@ -1,69 +1,114 @@
 import { Button, Grid, TextField, Typography } from "@mui/material";
 import useStyles from "./Style";
-import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { Link } from "react-router-dom";
 
 export default function SignUp() {
   const classes = useStyles();
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+
+  const userRegister = useSelector((state) => state.userRegister);
+  const { userInfo, error } = userRegister;
+
+  const dispatch = useDispatch();
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+  };
+
   const navigate = useNavigate();
 
   return (
-    <div className={classes.background}>
-      <Grid
-        containter
-        direction="column"
-        justifyContent="center"
-        alignItems="center"
-      >
-        <Grid item>
-          <Typography variant="h4" align="left" className={classes.title}>
-            Sign Up
-          </Typography>
-        </Grid>
-        <Grid item>
-          <Typography className={classes.subtitle} align="left">
-            Create account to start using Petstagram
-          </Typography>
-        </Grid>
-        <Grid item>
-          <Typography align="left" className={classes.body}>
-            Email
-          </Typography>
-        </Grid>
-        <Grid item align="left">
-          <TextField size="small" className={classes.inputBox} />
-        </Grid>
-        <Grid item>
-          <Typography align="left" className={classes.body}>
-            Password
-          </Typography>
-        </Grid>
-        <Grid item align="left">
-          <TextField size="small" className={classes.inputBox} />
-        </Grid>
-        <Grid item>
-          <Typography align="left" className={classes.body}>
-            Confirm Password
-          </Typography>
-        </Grid>
-        <Grid item align="left">
-          <TextField size="small" className={classes.inputBox} />
-        </Grid>
-        <Grid item align="left">
-          <Button variant="contained" size="large" className={classes.buttonS}>
-            Sign Up
-          </Button>
-        </Grid>
-        <Grid item align="left">
-          <Typography className={classes.haveacc}>
-            Already have an account?{" "}
-            <Link to="/signin" className={classes.link}>
-              Sign In
-            </Link>
-          </Typography>
-        </Grid>
-      </Grid>
+    <div>
+      <form onSubmit={submitHandler}>
+        <div className={classes.background}>
+          <Grid
+            containter
+            direction="column"
+            justifyContent="center"
+            alignItems="center"
+          >
+            <Grid item>
+              <Typography variant="h4" align="left" className={classes.title}>
+                Sign Up
+              </Typography>
+            </Grid>
+            <Grid item>
+              <Typography className={classes.subtitle} align="left">
+                Create account to start using Petstagram
+              </Typography>
+            </Grid>
+            <Grid item>
+              <Typography align="left" className={classes.body}>
+                Email
+              </Typography>
+            </Grid>
+            <Grid item align="left">
+              <TextField
+                type="email"
+                id="email"
+                required
+                size="small"
+                className={classes.inputBox}
+                onChange={(e) => setEmail(e.target.value)}
+              />
+            </Grid>
+            <Grid item>
+              <Typography align="left" className={classes.body}>
+                Password
+              </Typography>
+            </Grid>
+            <Grid item align="left">
+              <TextField
+                size="small"
+                className={classes.inputBox}
+                type="password"
+                id="password"
+                required
+                onChange={(e) => setPassword(e.target.value)}
+              />
+            </Grid>
+            <Grid item>
+              <Typography align="left" className={classes.body}>
+                Confirm Password
+              </Typography>
+            </Grid>
+            <Grid item align="left">
+              <TextField
+                size="small"
+                className={classes.inputBox}
+                type="password"
+                id="confirmPassword"
+                required
+                onChange={(e) => setConfirmPassword(e.target.value)}
+              />
+            </Grid>
+            <Grid item align="left">
+              <Button
+                variant="contained"
+                size="large"
+                className={classes.buttonS}
+                type="submit"
+              >
+                Sign Up
+              </Button>
+            </Grid>
+            <Grid item align="left">
+              <Typography className={classes.haveacc}>
+                Already have an account?{" "}
+                <Link to="/signin" className={classes.link}>
+                  Sign In
+                </Link>
+              </Typography>
+            </Grid>
+          </Grid>
+        </div>
+      </form>
     </div>
   );
 }
