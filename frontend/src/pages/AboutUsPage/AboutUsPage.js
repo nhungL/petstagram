37cd7f506a6de41bc3c6a46123a8270da232ef86
Truc Phan <pttruc.308@gmail.com>
2022-05-02 16:50:31 -0700

    add content for about us page

--- frontend/src/pages/AboutUsPage/AboutUsPage.js
+++ frontend/src/pages/AboutUsPage/AboutUsPage.js
@@ -8,19 +8,10 @@ export default function AboutUsPage() {
     <div className={classes.background}>
       <AppBarContent />
       <h1 className={classes.pageTitle}>About Us</h1>
-      <p className={classes.introText}>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras dictum
-        ultrices vestibulum. Nunc dui mi, maximus vitae dignissim quis, congue
-        ac orci. Cras ante orci, vulputate vitae maximus vel, dapibus non orci.
-        Aenean imperdiet sapien ac lectus ultricies mollis. Fusce purus leo,
-        scelerisque commodo dolor eu, aliquet commodo ex. Phasellus in sapien
-        lectus. Mauris vel sollicitudin nunc. Integer vulputate quam urna, vitae
-        mollis turpis bibendum eget. Mauris quis tortor auctor, mattis leo in,
-        luctus ligula. Donec facilisis nisl ut ligula tincidunt, tincidunt
-        fermentum ex laoreet. Nullam finibus diam a augue efficitur, vel
-        porttitor lacus faucibus. Donec eleifend consectetur euismod. Vivamus
-        sodales felis ut semper feugiat.
-      </p>
+      <h2 className={classes.introText}>
+        Our vision is to deliver a social media platform where pet owners can
+        connect together to create friendship for their pets ❤️
+      </h2>
       <hr style={{ width: "85%" }} />
       <div className={classes.pageBody}>
         <div className={classes.pageSection}>
@@ -35,11 +26,17 @@ export default function AboutUsPage() {
           >
             <h3 className={classes.name}>Thuy Le</h3>
             <p className={classes.description} style={{ textAlign: "left" }}>
-              Nullam sodales ante dui, eu fermentum orci consectetur a. Morbi
-              rhoncus libero quis gravida tincidunt. Orci varius natoque
-              penatibus et magnis dis parturient montes, nascetur ridiculus mus.
-              Fusce ac placerat mi. Mauris facilisis convallis auctor. Sed et
-              pharetra nibh, eu lacinia ante. Praesent eu elementum mi.
+              Hi, my name is Thuy Le! I have experience in building full-stack
+              web using the MERN framework (MongoDB, Express.js, React, and
+              Node.js). I am skilled in Java, C++, Swift (for iOS application),
+              HTML, CSS and I am learning Kotlin for Android applications.
+              Projects: I am collaborated with a team of 4 to launch an iOS app.
+              I was in charge of designing the UI/UX and worked mostly on the
+              backend of the application. I also have built an e-commerce
+              website as a personal project utilizing React/ Redux, Node.js,
+              Express, and MongoDB. I have experience working in teams which are
+              mostly from school projects. I value working in a team because we
+              can help and learn from each other.
             </p>
             <div
               style={{
@@ -79,11 +76,18 @@ export default function AboutUsPage() {
           >
             <h3 className={classes.name}>Nhung Luong</h3>
             <p className={classes.description} style={{ textAlign: "right" }}>
-              Nullam sodales ante dui, eu fermentum orci consectetur a. Morbi
-              rhoncus libero quis gravida tincidunt. Orci varius natoque
-              penatibus et magnis dis parturient montes, nascetur ridiculus mus.
-              Fusce ac placerat mi. Mauris facilisis convallis auctor. Sed et
-              pharetra nibh, eu lacinia ante. Praesent eu elementum mi.
+              Hi from a rising senior pursuing B.S. in Computer Science, minors
+              in Mathematics at San Jose State University. I have passion for
+              things that need critical thinking and logic, and I found Computer
+              Science as a good match for me. I love to build software
+              application that make positively impacts on people, and make our
+              lives happier! Day by day, I'm improving myself to become a better
+              software developer. This application starts from our love for
+              pets, and we'd love to create a platform for pet owners to
+              communicate as well as to make friends for their pets. Let us know
+              how you want to improve the app to meet your expectation via our
+              contact information. I also love to collaborate with people and
+              learn from others so let's connect!
             </p>
             <div
               style={{
@@ -125,11 +129,19 @@ export default function AboutUsPage() {
           >
             <h3 className={classes.name}>Truc Phan</h3>
             <p className={classes.description} style={{ textAlign: "left" }}>
-              Nullam sodales ante dui, eu fermentum orci consectetur a. Morbi
-              rhoncus libero quis gravida tincidunt. Orci varius natoque
-              penatibus et magnis dis parturient montes, nascetur ridiculus mus.
-              Fusce ac placerat mi. Mauris facilisis convallis auctor. Sed et
-              pharetra nibh, eu lacinia ante. Praesent eu elementum mi.
+              I'm a software engineer whose focus is full-stack development. I
+              am strongly driven by utilizing technology to better human lives
+              and I believe in building innovative products that many people
+              consider impossible! I have been working for Experian as a
+              software engineer intern since November 2021 and will be interning
+              at Momentive.ai in Summer 2022. Besides coding, love goring
+              thrifting both physically and virtually (I ❤️ Depop!), trying new
+              food/coffee/boba spots and taking candid pictures for the gram. I
+              also like being active such as hitting the gym and going on hikes.
+              For times when I feel a little potato, I’d prefer staying in,
+              watching shows, listening to music and cleaning my room. My
+              current goals right now are to adopt a cat, get better at cooking,
+              travel more... Did I already mention adopting a cat?
             </p>
             <div
               style={{
