87234d2bcfbce44ee3f98ced6fbabe52490afe0a
nhungL <nhung.luong1098@gmail.com>
2022-05-03 01:04:27 -0700

    fix styling for homepage

--- frontend/src/pages/HomePage/Style.js
+++ frontend/src/pages/HomePage/Style.js
@@ -2,9 +2,11 @@ import { makeStyles } from "@mui/styles";
 
 const useStyles = makeStyles((theme) => ({
   background: {
-    backgroundColor: "#F0F0F0",
     display: "flex",
     flexDirection: "column",
+    backgroundColor: "#F0F0F0",
+    alignItems: "center",
+    minHeight: "100vh",
   },
   avatar: {
     marginTop: "20px",
@@ -30,7 +32,7 @@ const useStyles = makeStyles((theme) => ({
   },
   container: {
     width: "100vw",
-    height: "100vh",
+    height: "100%",
   },
   chat: {
     backgroundColor: "#FFFFFF",
